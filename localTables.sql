cee51eb760a247677fe65d60ef19401f095a841c
williamkim0722 <william.eh.kim@gmail.com>
2017-05-01 13:32:04 -0400

    revised database information in sql file

--- localTables.sql
+++ localTables.sql
@@ -3,7 +3,7 @@ DROP TABLE orders;
 
 CREATE TABLE inventory(
        pid int8,
-       wid int8,
+       wid text, -- lists all warehouses that have this produce. e.g. if warehouse 1,2,3 have this, this field would be "1,2,3" where warehouses are separated by comma
        q int8,
        descr text
 );
@@ -12,13 +12,14 @@ CREATE INDEX items ON inventory (pid,descr);
 
 CREATE TABLE orders(
        pid int8,
-       wid int8,
+       wid text,
        q int8,
        descr text,
        status int4,
        track_no int8,
        adx int4,
-       ady int4
+       ady int4,
+       userID int4
 );
 
 CREATE INDEX orderID ON orders (pid,wid,status);
